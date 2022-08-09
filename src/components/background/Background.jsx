95f7b527f498ac7c7b571bee60f3ef496fd2b17e
skayl8827 <skayl8827@gmail.com>
2022-08-09 15:38:30 +0400

    replace video

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import classes from "./Background.module.css";
-import background from "./background.mp4";
+import background from "./BG.mp4";
 import backgroundWebm from "./bgvideo.webm";
 export default function Backgrond() {
   return (
     <>
       <video
         className={classes.bgVideo}
-        src={backgroundWebm}
         muted
         autoPlay
         loop
         playsInline
         preload="auto"
-      ></video>
+      >
+        <source
+          src={background}
+          type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+        />
+        <source src={backgroundWebm} type='video/webm; codecs="vp8, vorbis"' />
+      </video>
     </>
   );
 }
